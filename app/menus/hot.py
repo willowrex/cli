6d4891bd343db0cef15d24f010ad897b8920e7d9
willowrex <rheiwillow@gmail.com>
2025-10-11 09:30:13 +0700

    Update hot.py

--- app/menus/hot.py
+++ app/menus/hot.py
@@ -83,7 +83,7 @@ def show_hot_menu2():
         print("===================🔥 Paket  Hot 2 🔥==================")
         print("=======================================================")
         
-        url = "https://me.mashu.lol/pg-hot2.json"
+        url = "https://raw.githubusercontent.com/willowrex/engsel/refs/heads/main/json/test.json"
         response = requests.get(url, timeout=30)
         if response.status_code != 200:
             print("Gagal mengambil data hot package.")
